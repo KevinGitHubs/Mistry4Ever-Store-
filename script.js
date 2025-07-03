cfd2b6e5c384d12c6cc78e53750aa1d2bc2680b4
KevinGitHubs <103484316+KevinGitHubs@users.noreply.github.com>
2025-07-03 12:07:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ import { getFirestore, doc, addDoc, onSnapshot, collection, serverTimestamp } fr
 // --- KONFIGURASI FIREBASE ANDA ---
 // GANTI placeholder ini dengan kredensial Firebase Anda yang sebenarnya!
 const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-    projectId: "YOUR_FIREBASE_PROJECT_ID",
-    storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-    appId: "YOUR_FIREBASE_APP_ID",
-    measurementId: "YOUR_FIREBASE_MEASUREMENT_ID" // Opsional
+  apiKey: "AIzaSyCVoMytk0Xhqcmw8nQNXR6_lb0jLDXp-rY",
+  authDomain: "mistry4ever-store-ce987.firebaseapp.com",
+  projectId: "mistry4ever-store-ce987",
+  storageBucket: "mistry4ever-store-ce987.firebasestorage.app",
+  messagingSenderId: "52996287526",
+  appId: "1:52996287526:web:1a122e9afce5f5abddcae7",
+  measurementId: "G-N0VGECPQB8"
 };
 
 // Pastikan appId juga diambil dari config
